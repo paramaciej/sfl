fddba7b7371a14f532045204271e8b64363e8ded
Maciej Kacprzak <paramaciej@gmail.com>
2016-05-28 19:20:07 +0200

    Add divide by zero error

--- Exceptions/EvalErrors.hs
+++ Exceptions/EvalErrors.hs
@@ -3,12 +3,14 @@
 module Exceptions.EvalErrors where
 
 data EvalError
-    = NonExhaustivePatterns
+    = ZeroDivisionError
+    | NonExhaustivePatterns
     | LetPatternMismatch String
 
 
 instance Show EvalError where
     show = \case
+        ZeroDivisionError -> "Divide by zero!"
         NonExhaustivePatterns -> "Non-exhaustive patterns in match!"
         LetPatternMismatch val -> "Pattern matching error; the value of " ++ val ++
             " does not match with given pattern."
