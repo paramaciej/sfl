164edc14faf6f2b4345f66b058813ec1b42bdbd6
Maciej Kacprzak <paramaciej@gmail.com>
2016-05-29 10:00:43 +0200

    add red errors

--- Interpreter/Utils.hs
+++ Interpreter/Utils.hs
@@ -6,17 +6,19 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Interpreter.Evaluator
 import Interpreter.Types
+import Interpreter.Show
+import PrintSFL
+import System.Console.ANSI
 import TypeChecker.HindleyMilner
 import TypeChecker.Infer
 import TypeChecker.Types
 import TypeChecker.Utils
-import PrintSFL
 import TypeChecker.Show
 
 inferredType :: SFL.Exp -> PrSt Type
 inferredType e = do
     env <- gets types
-    let inferError tExp =  show tExp ++ "\nin expression:\n" ++ printTree e
+    let inferError tExp =  surroundSGR [SetColor Foreground Vivid Red] "\nType error:\n" ++ show tExp ++ "\nin expression:\n" ++ printTree e
     liftIO (runReaderT (runExceptT (infer $ tcExp e)) env) >>= either (throwError . inferError) (liftIO . zonk)
 
 typeStr :: SFL.Exp -> PrSt String
@@ -29,7 +31,7 @@ typeStr e = do
 evaluatedExp :: SFL.Exp -> PrSt Value
 evaluatedExp e = do
     env <- gets values
-    let evalError x = show x ++ "\nin expression:\n" ++ printTree e
+    let evalError x = surroundSGR [SetColor Foreground Vivid Red] "\nEvaluation error:\n" ++ show x ++ "\nin expression:\n" ++ printTree e
     liftIO (runReaderT (runExceptT (eval (tcExp e))) env) >>= either (throwError . evalError) return
 
 showExp :: SFL.Exp -> PrSt String
